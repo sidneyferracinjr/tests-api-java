b5ef9b8823f13de1d8f2c4dd2eeb43fe143047b8
sidneyferracinjr <sidneyferracinjr@gmail.com>
2024-10-15 14:34:06 -0300

    feat: domain class

--- src/main/java/br/com/testsapijava/api/ApiApplication.java
+++ src/main/java/br/com/testsapijava/api/ApiApplication.java
@@ -3,11 +3,15 @@ package br.com.testsapijava.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.testsapijava.api.domain.User;
+
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+		
+		User user = new User(1, "Sidney", "sidney@gmail.com", "123");
 	}
 
 }
